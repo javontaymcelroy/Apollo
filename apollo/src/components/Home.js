0e3e66a7f77b1d773d3dde75a641b8e50eb7d473
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-25 21:20:46 -0500

    Finished Home grid.

--- apollo/src/components/Home.js
+++ apollo/src/components/Home.js
@@ -17,7 +17,11 @@ class Home extends Component {
       abcnews: [],
       espn: [],
       techradar: [],
-      ign: []
+      ign: [],
+      mtv: [],
+      businessinsider: [],
+      time: [],
+      nationalgeographic: []
     };
   }
 
@@ -70,11 +74,49 @@ class Home extends Component {
       )
       .then(res => this.setState({ ign: res.data.articles[1] }))
       .catch(err => console.log(err));
+
+    // ------------------------------------- MTV NEWS ----------------------------------------------------- //
+    axios
+      .get(
+        'https://newsapi.org/v2/top-headlines?sources=mtv-news&apiKey=8d966ab2b0614bbebfb3425f0369bf0c'
+      )
+      .then(res => this.setState({ mtv: res.data.articles[1] }))
+      .catch(err => console.log(err));
+
+    // ------------------------------------- BUSINESS INSIDER ----------------------------------------------------- //
+    axios
+      .get(
+        'https://newsapi.org/v2/top-headlines?sources=business-insider&apiKey=8d966ab2b0614bbebfb3425f0369bf0c'
+      )
+      .then(res => this.setState({ businessinsider: res.data.articles[1] }))
+      .catch(err => console.log(err));
+
+    // ------------------------------------- TIME NEWS ----------------------------------------------------- //
+    axios
+      .get(
+        'https://newsapi.org/v2/top-headlines?sources=time&apiKey=8d966ab2b0614bbebfb3425f0369bf0c'
+      )
+      .then(res => this.setState({ time: res.data.articles[1] }))
+      .catch(err => console.log(err));
+
+    // ------------------------------------- NATIONAL GEOGRAPHIC ----------------------------------------------------- //
+    axios
+      .get(
+        'https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=8d966ab2b0614bbebfb3425f0369bf0c'
+      )
+      .then(res => this.setState({ nationalgeographic: res.data.articles[1] }))
+      .catch(err => console.log(err));
   }
 
   render() {
     const mashable = this.state.mashable;
     const wired = this.state.wired;
+    const espn = this.state.espn;
+    const mtv = this.state.mtv;
+    const businessinsider = this.state.businessinsider;
+    const time = this.state.time;
+    const nationalgeographic = this.state.nationalgeographic;
+
     return (
       <>
         <div className='hero'>
@@ -126,8 +168,44 @@ class Home extends Component {
             </div>
           </div>
         </div>
+        <div className='home-newsgrid'>
+          <span className='grid-main-overlay' />
+          <h1 className='grid-main-title'>{mtv.title}</h1>
+          <img src={mtv.urlToImage} alt={mtv.title} className='grid-main' />
+          <div className='quad-grid-info'>
+            <h4 className='grid-titles'>{espn.title}</h4>
+            <h4 className='grid-titles'>{businessinsider.title}</h4>
+            <h4 className='grid-titles'>{time.title}</h4>
+            <h4 className='grid-titles'>{nationalgeographic.title}</h4>
+          </div>
+          <div className='quad-grid'>
+            <img
+              src={espn.urlToImage}
+              alt={espn.title}
+              className='grid-image'
+            />
+            <img
+              src={businessinsider.urlToImage}
+              alt={businessinsider.title}
+              className='grid-image'
+            />
+            <img
+              src={time.urlToImage}
+              alt={time.title}
+              className='grid-image'
+            />
+            <img
+              src={nationalgeographic.urlToImage}
+              alt={nationalgeographic.title}
+              className='grid-image'
+            />
+          </div>
+        </div>
         <div className='background-elements'>
+          <span className='grid-background' />
+          <h1 className='news-header'>TOP NEWS</h1>
           <img src={subherobg} alt='border' className='subherobg' />
+          <span className='page-banner' />
           <span className='sub-div' />
           <span className='center-line' />
           <span className='left-line' />
